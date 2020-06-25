e69a0eb352d76ae3d612d6a1897b4c3a1440f9c2
Singham Lagatari <singham@friboemi.id>
2020-06-26 06:56:54 +0700

    Init Node App
    add boilerplate nodejs

---
+++ node-app/config/connection.js
@@ -0,0 +1,31 @@
+const development = {
+  database: 'efishery_dev',
+  username: 'sl',
+  password: '0605',
+  host: 'localhost',
+  dialect: 'postgres',
+};
+
+const testing = {
+  database: 'efishery_test',
+  username: 'sl',
+  password: '0605',
+  host: 'localhost',
+  dialect: 'postgres',
+};
+
+const production = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+};
+
+console.log(production)
+
+module.exports = {
+  development,
+  testing,
+  production,
+};
